60ef9fde27fb008ea7153204345230c7d782824a
edizzlla <edi.sulo01@gmail.com>
2025-05-08 13:37:52 +0200

    making some changes to the anchors to improve speed

---
+++ Code/Anchor1/sketch_apr22a/anchor1/anchor1.ino
@@ -0,0 +1,111 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <BLEDevice.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+#include <time.h>
+
+// const char* ssid = "HUAWEI-U8um";
+// const char* password = "TGJQV4ev";
+const char* ssid = "ICan";
+const char* password = "ican2022";
+//const char* serverUrl = "http://192.168.100.45:5000/api/rssi"; 
+const char* serverUrl = "http://192.168.100.53:5000/api/rssi"; 
+
+String anchorID = "Anchor1"; 
+String targetDeviceName = "SmartPhone"; 
+
+//int scanTime = 3;
+unsigned long lastScanTime = 0;
+BLEScan* pBLEScan;
+
+void connectToWiFi() {
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected!");
+}
+
+void sendRSSI(String mac, int rssi) {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(serverUrl);
+    http.addHeader("Content-Type", "application/json");
+    String payload = "{\"anchor_id\": \"" + anchorID + "\", \"mac\": \"" + mac + "\", \"rssi\": " + String(rssi) + "}";
+    int response = http.POST(payload);
+    Serial.print("POST response: ");
+    Serial.println(response);
+    http.end();
+  } else {
+    Serial.println("WiFi not connected!");
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  connectToWiFi();
+
+// NTP Time Sync
+  configTime(0, 0, "pool.ntp.org");
+  Serial.println("Waiting for NTP time sync...");
+  while (time(nullptr) < 100000) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nTime synchronized!");
+
+  BLEDevice::init("");
+  pBLEScan = BLEDevice::getScan();
+  pBLEScan->setActiveScan(true);
+}
+
+void loop() {
+  // time_t now = time(nullptr);
+  // struct tm* timeinfo = localtime(&now);
+
+  // Synchronize scan every 5 seconds (0, 5, 10, ...)
+ // if (timeinfo->tm_sec % scanTime == 0) {
+  if (millis() - lastScanTime >= 500) {
+    Serial.println("[" + anchorID + "] Starting synchronized scan!");
+
+    BLEScanResults results = *pBLEScan->start(1, false);
+    lastScanTime = millis();
+
+    bool found = false;
+
+    for (int i = 0; i < results.getCount(); i++) {
+      BLEAdvertisedDevice d = results.getDevice(i);
+
+      if (d.haveName() && d.getName() == targetDeviceName) {
+        String mac = String(d.getAddress().toString().c_str());
+        int rssi = d.getRSSI();
+
+        Serial.println("[" + anchorID + "] Found target by name!");
+        Serial.println("Name: " + String(d.getName().c_str()));
+        Serial.println("MAC: " + mac);
+        Serial.println("RSSI: " + String(rssi));
+        // Serial.println("Time: " + String(timeinfo->tm_sec));
+        sendRSSI(mac, rssi);
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      Serial.println("[" + anchorID + "] Target not found");
+    }
+
+    pBLEScan->clearResults();
+    //delay(1200); // avoid multiple scans in the same second
+  }
+
+  //delay(200); // check again soon
+}
+
+
+
+
+
