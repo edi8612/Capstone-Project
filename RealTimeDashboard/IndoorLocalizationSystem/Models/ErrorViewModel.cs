4afcd33f31fdbc85549e0d0422707924834e9349
edizzlla <edi.sulo01@gmail.com>
2025-06-05 01:49:38 +0200

    Removed .net dashboard

--- RealTimeDashboard/IndoorLocalizationSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace IndoorLocalizationSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
